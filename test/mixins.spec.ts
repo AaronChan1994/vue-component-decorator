b233cc536efe499857e142fb795b72aa4c5f8f8d
AaronChan1994 <tech_aaron@foxmail.com>
2023-10-23 08:51:58 +0800

    modify mixins

--- test/mixins.spec.ts
+++ test/mixins.spec.ts
@@ -1,44 +1,52 @@
 require('./jsdom.init');
-import { mount } from '@vue/test-utils';
-import { expect } from 'chai';
-import { component, Vue } from '../src';
-import { mixins } from '../src/utils';
+import {mount} from '@vue/test-utils';
+import {expect} from 'chai';
+import {component} from '../src';
+import {mixins} from '../src/utils';
 
 class A {
-  public a = 'a';
-  public c = 'a';
+    public a: string;
 
-  public aa() {
-    return this.a;
-  }
+    public constructor(a: string) {
+        this.a = a;
+    }
+
+    public aa() {
+        return this.a;
+    }
 }
 
-class B {
-  public b = 'b';
-  public c = 'b';
+class B extends A {
+    public b: string;
 
-  public bb() {
-    return this.b;
-  }
+    public constructor() {
+        super('a');
+        this.b = 'b';
+    }
+
+    public bb() {
+        return this.b;
+    }
 }
 
-class C extends mixins(Vue, A, B) {
-  public c = 'c';
+class C {
+    public c = 'c';
 }
 
-@component()
-class Component extends C {
-  public get foo() {
-    return this.c;
-  }
+
+@component
+class Component extends mixins(B, C) {
+    public get foo() {
+        return this.c;
+    }
 }
 
 describe('mixins.ts', () => {
-  it('should mixins all members', () => {
-    const ClassComponent: any = Component;
-    const vm = mount<any>(ClassComponent).vm;
-    expect(vm.a).to.equal(vm.aa()).to.equal('a');
-    expect(vm.b).to.equal(vm.bb()).to.equal('b');
-    expect(vm.c).to.equal(vm.foo).to.equal('c');
-  });
+    it('should mixins all members', () => {
+        const ClassComponent: any = Component;
+        const vm = mount<any>(ClassComponent).vm;
+        expect(vm.a).to.equal(vm.aa()).to.equal('a');
+        expect(vm.b).to.equal(vm.bb()).to.equal('b');
+        expect(vm.c).to.equal(vm.foo).to.equal('c');
+    });
 });
